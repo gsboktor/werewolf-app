4d3c6d9c4dde77aace9b42269fa472db0847be04
Nibraas Khan <nnibraas@gmail.com>
2020-06-21 21:00:49 -0400

    Added some functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,26 @@
-import * as functions from 'firebase-functions';
-
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+
+admin.initializeApp();
+
+const firestore = admin.firestore();
+
+export const helloWorld = functions.https.onRequest((request: any, response: any) => {
+    console.log("Hello World Here");
+ response.send("Hello from Firebase!");
+});
+
+exports.setRoles = functions.firestore.document('Games/{gameID}').onUpdate((change: any, context: any) => {
+    console.log('Set Roles Here');
+})
+
+exports.getWatsonTokenFS = functions.firestore
+  .document('users/{userId}/{messageCollectionId}/{messageId}')
+  .onWrite((snap: any, context: any) => {
+    console.log("here from get watson");
+    return firestore.doc(`IBM_Watson_Token/Token_Value`).update('token');
+});
