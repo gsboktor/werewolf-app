1ad9d9da8474b9d860c4ced79e99b1eb557dfa1c
Nibraas Khan <nnibraas@gmail.com>
2020-06-28 19:04:42 -0400

    Set roles function working

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,54 @@
+// Set up admin
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// Init functions and database
 const functions = require('firebase-functions');
+const db = admin.firestore();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+// Sets the roles for each player for each game ID
+function setRoles(gameID){
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+    // Set reference to doc with game ID
+    const docRef = db.collection(gameID).doc('playerRoles');
+
+    // Get all the information nessesary and then set the roles
+    docRef.get().then(doc => {
+
+        // Get the current doc data
+        var data = doc.data();
+
+        // Dict for roles for each user
+        var userRoles = {}
+
+        // Get all the roles, the users, and the host
+        var roles = data['roles'];
+        var host = data['host'];
+        usersArr = Object.keys(data['users']);
+        usersArr = usersArr.filter(function(e) { return e !== host });
+
+        // Assign each role to a user
+        for (var key in roles) {
+            for (i = 0; i < roles[key]; i++) {
+                u = usersArr[Math.floor(Math.random() * usersArr.length)];
+                usersArr = usersArr.filter(function(e) { return e !== u });
+                userRoles[u] = [key];
+            }
+        }
+
+        // Update the roles map
+        db.collection(gameID).doc('playerRoles').update({
+            userRoles: userRoles
+        });
+    });
+}
+
+// Cloud function to set the roles of each game ID
+exports.seeChanges = functions.firestore.document('{gameID}/gameRules').onUpdate((change, context) => {
+
+    // Make sure it's time to set the roles and set them
+    const body = change.after.data()
+    if (body['setRoles'] == true){
+        setRoles(context.params.gameID)
+    }
+})
