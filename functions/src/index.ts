41b60c146920cc7e0f22989e4ebd8525348ff704
Nibraas Khan <nnibraas@gmail.com>
2020-06-28 17:02:51 -0400

    Typescript functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,19 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
+const db = admin.firestore();
 
 admin.initializeApp();
+
+function readRolesData(gameID: String){
+  const docRef = db.collection(gameID).doc('playerRoles');
+  docRef.get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log('No such document!');
+      } 
+      console.log(doc.data());
+    });
+}
 
 export const helloWorld = functions.https.onRequest((request: any, response: any) => {
     console.log("Hello World Here");
@@ -10,4 +22,12 @@ export const helloWorld = functions.https.onRequest((request: any, response: any
 
 exports.setRoles = functions.firestore.document('{gameID}/{userID}').onUpdate((change: any, context: any) => {
   console.log(context.params.gameID, context.params.userID);
+})
+
+exports.seeChanges = functions.firestore.document('{gameID}/gameRules').onUpdate((change: any, context: any) => {
+  const body = change.after.data()
+  if (body['setRoles'] == true){
+      readRolesData(context.params.gameID)
+      console.log("here set roles changed");
+  }
 })
